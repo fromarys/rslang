5a3518f72c95b7e98d511bf2533579f64299d879
Andronio <gprsboost03@mail.ru>
2022-08-22 23:45:25 +0600

    fix: remove quotation from interfaces

--- src/app/basic/interfaces/interfaces.ts
+++ src/app/basic/interfaces/interfaces.ts
@@ -24,33 +24,33 @@ export interface IError {
 }
 
 export interface IUserWord {
-  difficulty: 'string';
+  difficulty: string;
   optional: Record<string, string>;
 }
 
 export interface IWord {
-  id: 'string';
+  id: string;
   group: number;
   page: number;
-  word: 'string';
-  image: 'string';
-  audio: 'string';
-  audioMeaning: 'string';
-  audioExample: 'string';
-  textMeaning: 'string';
-  textExample: 'string';
-  transcription: 'string';
-  wordTranslate: 'string';
-  textMeaningTranslate: 'string';
-  textExampleTranslate: 'string';
+  word: string;
+  image: string;
+  audio: string;
+  audioMeaning: string;
+  audioExample: string;
+  textMeaning: string;
+  textExample: string;
+  transcription: string;
+  wordTranslate: string;
+  textMeaningTranslate: string;
+  textExampleTranslate: string;
 }
 
 export interface IStatistic {
-  learnedWords: 'number';
+  learnedWords: number;
   optional: Record<string, string>;
 }
 
 export interface ISetting {
-  wordsPerDay: 'number';
+  wordsPerDay: number;
   optional: Record<string, string>;
 }
