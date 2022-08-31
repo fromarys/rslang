cb5449d222a9cf1f63ebdb27b6f0ab7862b4c7ce
Oksana <seregina.oksana.1987@gmail.com>
2022-09-01 00:09:18 +0600

    feat: add links on main page and del custom event

--- src/app/basic/events/statistics-click.event.ts
+++
@@ -1,3 +0,0 @@
-import { IHeaderMenu } from './models';
-
-export const STATISTICS_CLICK_EVENT = new Event(IHeaderMenu.Statistics);
--- src/app/components/CardOpportunities/card-opportunities.view.ts
+++ src/app/components/CardOpportunities/card-opportunities.view.ts
@@ -2,12 +2,14 @@ import './card-opportunities.scss';
 import { ICardOpportunities } from './models';
 
 export function getTemplate(settings: ICardOpportunities): string {
-  return `<div class="card-opportunities">
+  return `<a href="#${settings.route}">
+            <div class="card-opportunities">
             <img class="card-backgr" src="${settings.srcBackgr}" alt="background" />
-            <div class="content-card">
-              <img class="icon-opportunities" src="${settings.srcIcon}" alt="textbook" />
-              <p class="text-opportunities">${settings.text}</p>
+              <div class="content-card">
+                <img class="icon-opportunities" src="${settings.srcIcon}" alt="textbook" />
+                <p class="text-opportunities">${settings.text}</p>
+              </div>
             </div>
-          </div>
+          </a>
   `;
 }
